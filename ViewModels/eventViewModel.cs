0b5a6c51cccccf3e74c32d5b338a00f733e878e7
Kyle Randall <kyle.randall@xtratyme.com>
2019-11-03 16:53:58 -0600

    Added the backend for basic listing of events.

---
+++ ViewModels/eventViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ticketswap.ViewModels
+{
+    public class eventViewModel
+    {
+        public int EVENT_ID { get; set; }
+
+        public string EVENT_DATE { get; set; }
+
+        public string EVENT_TIME { get; set; }
+
+        public string EVENT_VENUE { get; set; }
+
+        public string EVENT_ADDRESS { get; set; }
+
+        public string EVENT_CITY { get; set; }
+
+        public string EVENT_STATE { get; set; }
+
+        public int? EVENT_ZIP { get; set; }
+
+        public string EVENT_COUNTRY { get; set; }
+
+        public string EVENT_ICON_LOCATION { get; set; }
+
+        public string EVENT_DESCRIPTION { get; set; }
+
+        public int? CATEGORY_ID { get; set; }
+
+    }
+}
